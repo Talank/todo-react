9184289c0971320574135b21a71f464eff45e6c4
talank <talank@gces.edu.np>
2020-07-01 14:01:07 +0545

    Added todo app and test for add task

---
+++ README.md
@@ -0,0 +1,19 @@
+# To-Do-App-in-React
+This is a simple To Do Application in react with tests written in Jest. This application is created for those who wants to learn about tesing with jest.
+
+## Install and run the project:
+- Clone the project from github
+- Go to the project directory and run the command:
+    ```
+    npm install
+    ```
+- Run the application using the following command
+    ```
+    npm run start
+    ```
+
+## Run the test:
+Use the following command to run the test;
+```
+npm run test
+```
---
+++ src/__tests__/addToDo.test.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+
+describe('Todo React', () => {
+    let browser
+    let page
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch()
+    })
+
+    afterEach(async () => {
+        await page.close()
+      })
+    
+      afterAll(async () => {
+        await browser.close()
+      })
+
+      describe('add task on the list', () => {
+        beforeEach(async () => {
+          page = await browser.newPage()
+          await page.goto('http://localhost:3000')
+        })
+
+      it('should be possible to add task to the list', async () => {
+          const taskInputField = await page.$x('//input[@placeholder="Enter task"]')
+          const taskToAdd = "New Task"
+          await taskInputField[0].click() 
+          await taskInputField[0].type(taskToAdd)
+
+          await page.keyboard.press('Enter')
+
+          const lists = await page.$x("//div[@class='list']/p/input");
+
+          let toDo;
+
+          for( let list of lists ) {
+            toDo = await page.evaluate(el => el.getAttribute("value"), list);
+          }
+          
+          expect(toDo).toBe(taskToAdd)
+        })
+      })
+})
